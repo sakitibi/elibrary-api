3f3f909fab9ba0d82048de2e95fbbadfd9d29291
sakitibi <sakitibi10nin@icloud.com>
2026-04-08 22:40:50 +0900

    Merge remote changes

--- js/77a4be0d-62cf-236b-71d8-a85aef088d9e.js
+++ js/77a4be0d-62cf-236b-71d8-a85aef088d9e.js
@@ -1,11 +1,14 @@
 window.decodeBase64Unicode = function(base64) {
     try {
+        console.log("b64 input: ", base64);
         const binaryString = atob(base64);
         const bytes = new Uint8Array(binaryString.length);
         for (let i = 0; i < binaryString.length; i++) {
             bytes[i] = binaryString.charCodeAt(i);
         }
-        return new TextDecoder("utf-8").decode(bytes);
+        const decoded = new TextDecoder("utf-8").decode(bytes);
+        console.log("b64 decoded: ", decoded);
+        return decoded;
     } catch (e) {
         return "Decode Error";
     }
